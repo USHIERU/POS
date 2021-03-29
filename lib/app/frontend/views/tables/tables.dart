24ee3bab7af92e6324e79a2e3c21315b37f4d229
ushieru <uzielcocolan@gmail.com>
2021-03-28 18:36:50 -0600

    add table

---
+++ lib/app/frontend/views/tables/tables.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class Tables extends StatefulWidget {
+  @override
+  _TablesState createState() => _TablesState();
+}
+
+class _TablesState extends State<Tables> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
