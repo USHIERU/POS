874e1ab57440d30291c4556702b310735f6ad641
ushieru <uzielcocolan@gmail.com>
2021-06-12 12:05:50 -0500

    fix bug (re open box)

--- lib/context/shared/infrastructure/persistence/hive/hive_repository.dart
+++ lib/context/shared/infrastructure/persistence/hive/hive_repository.dart
@@ -1,13 +1,17 @@
 import 'package:hive/hive.dart';
 
 abstract class HiveRepository<T> {
-  HiveRepository(
-      String boxName, TypeAdapter<T> typeAdapter, {void Function()? callback}) {
-    Hive.registerAdapter<T>(typeAdapter);
-    print('[HIVE 🍯] Adapter [${typeAdapter.runtimeType}] registered');
-    Hive.openBox<T>(boxName).then((_) {
-      print('[HIVE 🍯] $boxName OPEN');
-      if (callback != null) callback();
-    });
+  HiveRepository(String boxName, TypeAdapter<T> typeAdapter,
+      {void Function()? callback}) {
+    if (!Hive.isAdapterRegistered(typeAdapter.typeId)) {
+      Hive.registerAdapter<T>(typeAdapter);
+      print('[HIVE 🍯] Adapter [${typeAdapter.runtimeType}] registered');
+    }
+    if (!Hive.isBoxOpen(boxName)) {
+      Hive.openBox<T>(boxName).then((_) {
+        print('[HIVE 🍯] $boxName OPEN');
+        if (callback != null) callback();
+      });
+    }
   }
 }
