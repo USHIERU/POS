3248a169cb488588108f156aa5b77bcae1756ebc
ushieru <uzielcocolangmail.com>
2021-06-19 14:59:34 -0500

    fix abstract class

--- lib/context/product/domain/product_repository.dart
+++ lib/context/product/domain/product_repository.dart
@@ -1,6 +1,6 @@
 import 'package:pos/context/product/domain/product.dart';
 
 abstract class ProductRepository {
-  Future<Product> save(Product product) => Future.value(product);
-  Future<List<Product>> getProducts() => Future.value([Product.empty()]);
+  Future<Product> save(Product product);
+  Future<List<Product>> getProducts();
 }
