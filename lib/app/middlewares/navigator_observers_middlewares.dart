4eecf1b9265311ed157a5724217b885764714a18
Ushieru <uzielcocolan@gmail.com>
2021-07-08 16:02:47 -0500

    use factory

--- lib/app/middlewares/navigator_observers_middlewares.dart
+++ lib/app/middlewares/navigator_observers_middlewares.dart
@@ -1,13 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:pos/app/configs/pos_config.dart';
 import 'package:pos/app/routes/login/login_screen.dart';
 import 'package:pos/context/session/application/get_session.dart';
-import 'package:pos/context/session/infrastructure/persistence/session_in_memory.dart';
 
 class AuthMiddleware extends GetMiddleware {
   @override
   RouteSettings? redirect(String? route) {
-    if (GetSession(SessionInMemory()).run() == null) {
+    if (GetSession(POSConfig().factory.getSessionRepository).run() == null) {
       return RouteSettings(name: LoginScreen.routeName);
     }
     return null;
